bbb236fdf8335fb11098daa53eead7d258aea717
Claude Ni <49127719+ClaudeNi@users.noreply.github.com>
2021-12-30 11:31:22 +0200

    Add files via upload

---
+++ src/components/GameBoard/GameBoard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Player from "../Player/Player";
+
+const GameBoard = () => {
+    return (
+        <div>
+            <Player />
+        </div>
+    );
+};
+
+export default GameBoard;
---
+++ src/components/Player/Player.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./player.css";
+
+const Player = () => {
+    return (
+        <div className="player-container">
+            <div className="player"></div>
+        </div>
+    );
+};
+
+export default Player;
