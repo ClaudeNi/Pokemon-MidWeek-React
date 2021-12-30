ba6bb80345b3947aa2a0efa65764f7ee92e677f7
Claude Ni <49127719+ClaudeNi@users.noreply.github.com>
2021-12-30 20:44:38 +0200

    Add files via upload

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./homepage.css";
 
 const HomePage = () => {
-    return <div className="homepage">Homepage</div>;
+    return (
+        <div className="homepage">
+            Homepage
+            <Link to="/game"></Link>
+        </div>
+    );
 };
 
 export default HomePage;
