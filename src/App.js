44e4a09af93d8cb662a395d3c892c7f1534b7614
ClaudeNi <claudenij2@gmail.com>
2022-09-05 18:19:19 +0300

    removed normal page

--- src/App.js
+++ src/App.js
@@ -3,35 +3,32 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage/Homepage";
 import TitleScreen from "./pages/TitleScreen/TitleScreen";
 import TeamBuilderPage from "./pages/TeamBuilderPage/TeamBuilderPage";
-import GameBoard from "./components/GameBoard/GameBoard";
 import BattleBoard from "./components/BattleBoard/BattleBoard";
-// import Map from "./components/Map/Map";
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
 import "./App.css";
 
 function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <Switch>
-                    <Route path="/" exact component={Homepage} />
-                    <Route path="/title" exact component={TitleScreen} />
-                    <Route
-                        path="/teamBuilder"
-                        exact
-                        component={TeamBuilderPage}
-                    />
-                    <Route path="/game/:mapName" exact component={GameBoard} />
-                    <Route
-                        path="/game/battle/:mapName"
-                        exact
-                        component={BattleBoard}
-                    />
-                    <Route component={PageNotFound} />
-                </Switch>
-            </BrowserRouter>
-        </div>
-    );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Switch>
+					<Route path="/" exact component={Homepage} />
+					<Route path="/title" exact component={TitleScreen} />
+					<Route
+						path="/teamBuilder"
+						exact
+						component={TeamBuilderPage}
+					/>
+					<Route
+						path="/game/battle/:mapName"
+						exact
+						component={BattleBoard}
+					/>
+					<Route component={PageNotFound} />
+				</Switch>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
